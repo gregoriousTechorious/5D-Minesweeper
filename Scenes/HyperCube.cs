c3ce98e0403ff92266187a907725858b90ae8097
Michael X <michael.xuereb85@gmail.com>
2024-10-03 19:59:42 +0100

    Worked on Main Menu a bit and started on the objects

---
+++ Scenes/HyperCube.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class HyperCube : Node3D
+{
+
+	int[,] adjacentPositions = new int[243, 5];
+	bool isBomb;
+	int[] position = new int[5];
+
+
+	public HyperCube(int[] pos)
+	{
+		this.position = pos;
+		Console.WriteLine("new hypercube");
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -5,7 +5,23 @@ public partial class MainMenu : Control
 {
 	private void OnPlayPressed()
 	{
-		GetTree().ChangeSceneToFile("res://Scenes/game.tscn");
+		try 
+		{
+			int dim1 = int.Parse(GetNode<LineEdit>("MarginContainer/GridContainer/HBoxContainer2/Dimension1").Text);
+			int dim2 = int.Parse(GetNode<LineEdit>("MarginContainer/GridContainer/HBoxContainer2/Dimension2").Text);
+			int dim3 = int.Parse(GetNode<LineEdit>("MarginContainer/GridContainer/HBoxContainer2/Dimension3").Text);
+			int dim4 = int.Parse(GetNode<LineEdit>("MarginContainer/GridContainer/HBoxContainer2/Dimension4").Text);
+			int dim5 = int.Parse(GetNode<LineEdit>("MarginContainer/GridContainer/HBoxContainer2/Dimension5").Text);
+			GetTree().ChangeSceneToFile("res://Scenes/game.tscn");
+		}
+		catch (System.FormatException e)
+		{
+			Console.WriteLine(e);
+			// this needs to make a popup to user that they did not input a number
+		}
+		
+		
+		
 	}
 	
 	private void OnOptionsPressed()
